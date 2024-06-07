dd7911a30ca31962d046dc0b90a0350bc3b49b57
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-06-07 13:34:41 +0700

    feat: Fetching Product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,58 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [product, setProduct] = useState([]);
+
+  const getProduct = async () => {
+    try {
+      const product = await axios.get("http://localhost:4001/products");
+      setProduct(product.data.data);
+    } catch (error) {
+      console.log("Error fetching the products", error);
+    }
+  };
+
+  const removeProduct = (index) => {
+    let deleteProduct = [...product];
+    deleteProduct.splice(index, 1);
+    setProduct(deleteProduct);
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
+        {product.map((product, index) => (
+          <div className="product" key={product.id}>
+            <div className="product-preview">
+              <img
+                src={product.image}
+                alt={product.name}
+                width="350"
+                height="350"
+              />
+            </div>
+            <div className="product-detail">
+              <h1>Product name: {product.name}</h1>
+              <h2>Product price: {product.price} Baht</h2>
+              <p>Product description: {product.description}</p>
+            </div>
+            <button
+              className="delete-button"
+              onClick={() => removeProduct(index)}
+            >
+              x
+            </button>
           </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
-        </div>
+        ))}
       </div>
     </div>
   );
