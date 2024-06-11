c08f97792fc57a89f9ac31b1f73bebd38663d96a
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-06-11 21:39:21 +0700

    Implement product fetching and deletion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,53 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getPop();
+  }, []);
+
+  const getPop = async () => {
+    const getPopimi = await axios.get("http://localhost:4001/products");
+    setData(getPopimi.data.data);
+    console.log(getPopimi.data.data)
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:4001/products/${id}`)
+    getPop();
+  }
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+      {data.map((item) => {
+        return (
+          <div className="product-list" key={item.id}>
+            <div className="product">
+              <div className="product-preview">
+                <img
+                  src={item.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
-      </div>
+              <div className="product-detail">
+                <h1>Product name: {item.name}</h1>
+                <h2>Product price:{item.price} </h2>
+                <p>Product description: {item.description}</p>
+              </div>
+              <button className="delete-button" onClick={(event) => {handleDelete(item.id)}}>x</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
